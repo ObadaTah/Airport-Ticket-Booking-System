fa31fdf6e7ac4e310b98b33c3b6ade5dd9daa8f9
ObadaTah <obadatahboub20@gmail.com>
2024-08-22 11:51:35 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Airport_Ticket_Booking_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
