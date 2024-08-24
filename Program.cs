ee81f02651672e9903f9cd9b2a505849305dfe45
ObadaTah <obadatahboub20@gmail.com>
2024-08-24 23:39:07 +0300

    added functionality to add headers automatically for files for readiness

--- Program.cs
+++ Program.cs
@@ -9,8 +9,9 @@ public class Program
     {
 
         string[] files = { "users.csv", "flights.csv", "bookings.csv" };
-        FileSystemUtilites.InitFiles(files);
-
+        string[] fileHeaders = { "username,password,role", "flightNumber,origin,destination,departureTime,arrivalTime,price", "bookingId,flightNumber,username" };
+        FileSystemUtilites.InitFiles(files, fileHeaders);
+        
         int choice = GenericUtilites.promptLoginRegister();
         switch (choice)
         {
--- Utilites/FileSystemUtilites.cs
+++ Utilites/FileSystemUtilites.cs
@@ -18,12 +18,42 @@ public static class FileSystemUtilites
         }
     }
 
-    public static void InitFiles(string[] files)
+    public static void InitFiles(string[] files, string[] fileHeaders)
     {
         for (int i = 0; i < files.Length; i++)
         {
             CreateFile(files[i]);
         }
+
+        for (int i = 0; i < files.Length; i++)
+        {
+            if (checkIfFileEmpty(files[i]))
+            {
+                WriteToFile(files[i], fileHeaders[i]);
+            }
+        }
+    }
+
+    public static void WriteToFile(string fileName, string data)
+    {
+        string path = Path.Combine(GetPath(), "storage");
+        path = Path.Combine(path, fileName);
+        using (StreamWriter sw = File.AppendText(path))
+        {
+            sw.WriteLine(data);
+        }
+    }
+
+    public static Boolean checkIfFileEmpty(string fileName)
+    {
+        string path = Path.Combine(GetPath(), "storage");
+        path = Path.Combine(path, fileName);
+        FileInfo fi = new FileInfo(path);
+        if (fi.Length == 0)
+        {
+            return true;
+        }
+        return false;
     }
 
 }
